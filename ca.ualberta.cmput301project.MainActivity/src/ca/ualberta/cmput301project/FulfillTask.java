a5651bafee970929accb4935aef04e1d3be167df
pqin <pqin@ualberta.ca>
2012-11-04 14:34:05 -0700

    fulfill activity shows tasks reqs

--- ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/cmput301project/FulfillTask.java
+++ ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/cmput301project/FulfillTask.java
@@ -1,17 +1,44 @@
 package ca.ualberta.cmput301project;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 import android.app.Activity;
+import android.content.Intent;
 
-public class FulfillTask extends Activity {
+public class FulfillTask extends Activity implements OnClickListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fulfilltask);
         
+        //Get task description from intent
+        Intent intent = getIntent();
+        String requestDescription = intent.getStringExtra(ViewLocalTask.EXTRA_MESSAGE);
+        
+        TextView requirements = (TextView) findViewById(R.id.requirements);
+        requirements.setText(requestDescription);
     }
     
-    
-
+    public void onClick(View v){
+    	Intent intent;
+    	switch (v.getId()){
+    		case R.id.get_audio:
+    			break;
+    		case R.id.get_image:
+    			break;
+    		case R.id.taskdone:
+    			//save changes
+    			//return to list
+    			break;
+    		case R.id.save_draft:
+    			//save changes
+    			//return to list
+    			break;
+    		default:
+    			return;
+    	}
+    }
 }
--- ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/cmput301project/ViewLocalTask.java
+++ ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/cmput301project/ViewLocalTask.java
@@ -8,15 +8,26 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ViewLocalTask extends ListActivity{
+	public final static String EXTRA_MESSAGE = "ca.ualberta.cmput301project.MESSAGE";
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasklist);
+        
         setListAdapter(new ArrayAdapter<Task>(this,android.R.layout.simple_list_item_1,MainActivity.locallist));
-
     }
     public void onListItemClick(ListView parent,View v, int position,long id){
-        MainActivity.list_index = position;
+    	//list index set to position of clicked entry
+    	MainActivity.list_index = position;
+    	//find task that was clicked
+    	Task clickedTask = MainActivity.thelist.get(MainActivity.list_index);
+    	//get task requirements
+    	String requirements = clickedTask.getDescription();
+    	
         Intent intent = new Intent (this, FulfillTask.class);
+        //send requirements with intent
+        intent.putExtra(EXTRA_MESSAGE, requirements);
+        
         startActivity(intent);
-        finish();
     }
 }
