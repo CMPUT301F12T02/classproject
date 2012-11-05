51b54cae08d656deb1a699de8e4dec40d33906e4
kerr2 <kerr2@ualberta.ca>
2012-11-05 09:22:10 -0700

    saving changes to External Tasks

--- ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/cmput301project/ViewExternalTask.java
+++ ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/cmput301project/ViewExternalTask.java
@@ -1,44 +1,56 @@
 package ca.ualberta.cmput301project;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.os.Bundle;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ViewExternalTask extends ListActivity{
-        public final static String EXTRA_MESSAGE = "ca.ualberta.cmput301project.MESSAGE";
+        public final static String REQDESCRIPTION = "ca.ualberta.cmput301project.DESCRIPTION";
+        public final static String REQPHOTO = "ca.ualberta.cmput301project.PHOTO";
+        public final static String REQAUDIO = "ca.ualberta.cmput301project.AUDIO";
+        //public final static String EXTRA_MESSAGE = "ca.ualberta.cmput301project.MESSAGE";
         
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasklist);
-        Task[] taskList = readAllExtTasks();
         
-        setListAdapter(new ArrayAdapter<Task>(this,android.R.layout.simple_list_item_1, Arrays.asList(taskList)));
+        refresh();
     }
+    
+    public void onRestart(Bundle savedInstanceState) {
+        super.onRestart();
+        refresh();
+    }
+    
+    private void refresh() {
+        ArrayList<Task> tasks = readAllExtTasks();
+        setListAdapter(new ArrayAdapter<Task>(this,android.R.layout.simple_list_item_1,tasks));
+    }
+    
     public void onListItemClick(ListView parent,View v, int position,long id){
-        //list index set to position of clicked entry
-        MainActivity.list_index = position;
-        //find task that was clicked
-        Task clickedTask = MainActivity.thelist.get(MainActivity.list_index);
-        //get task requirements
-        String requirements = clickedTask.getDescription();
+        ArrayList<Task> tasks = readAllExtTasks();
+        Task clickedTask = tasks.get(position);
         
-        Intent intent = new Intent (this, FulfillTask.class);
-        //send requirements with intent
-        intent.putExtra(EXTRA_MESSAGE, requirements);
-        
-        startActivity(intent);
+        Intent intent = new Intent();
+        Bundle b = new Bundle();
+                b.putSerializable("task", clickedTask);
+                intent.putExtras(b);
+                intent.setClass(this, FulfillTask.class);
+                startActivity(intent);
     }
-    private Task[] readAllExtTasks(){
+    private ArrayList<Task> readAllExtTasks(){
+        ArrayList<Task> tasks = new ArrayList<Task>();
         JSONArray jarray = null;
         try
         {
@@ -47,7 +59,6 @@ public class ViewExternalTask extends ListActivity{
         {
             e.printStackTrace();
         }
-        Task[] taskList = new Task[jarray.length()+1];
         for (int i = 0; i < jarray.length()+1; i++){
             try
             {
@@ -58,12 +69,12 @@ public class ViewExternalTask extends ListActivity{
                 boolean reqAudio = Boolean.valueOf(content.getString("reqAudio"));
                 Date timestamp = (Date)content.get("timestamp");
                 Task task = new Task(description, reqPhoto, reqAudio, timestamp);
-                taskList[i] = task;
+                tasks.add(task);
             } catch (JSONException e)
             {
                 e.printStackTrace();
             }
         }
-        return taskList;
+        return tasks;
     }
 }
