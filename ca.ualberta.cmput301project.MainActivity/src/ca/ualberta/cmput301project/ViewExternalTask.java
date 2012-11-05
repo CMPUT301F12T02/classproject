dbf863b911e127b60d6403f5d1b2e10a61c62897
Peter Qin <pqin@ualberta.ca>
2012-11-05 09:59:05 -0700

    fixed bunch of eclipse errors

--- ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/cmput301project/ViewExternalTask.java
+++ ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/cmput301project/ViewExternalTask.java
@@ -2,8 +2,6 @@ package ca.ualberta.cmput301project;
 
 import java.sql.Date;
 import java.util.ArrayList;
-import java.util.Arrays;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,8 +30,8 @@ public class ViewExternalTask extends ListActivity{
         super.onRestart();
         refresh();
 
-        Task[] taskList = readAllExtTasks();
-        setListAdapter(new ArrayAdapter<Task>(this,android.R.layout.simple_list_item_1, Arrays.asList(taskList)));
+        ArrayList<Task> taskList = readAllExtTasks();
+        setListAdapter(new ArrayAdapter<Task>(this,android.R.layout.simple_list_item_1, (Task[]) taskList.toArray()));
     }
     
     private void refresh() {
@@ -48,36 +46,10 @@ public class ViewExternalTask extends ListActivity{
         
         Intent intent = new Intent();
         Bundle b = new Bundle();
-                b.putSerializable("task", clickedTask);
-                intent.putExtras(b);
-                intent.setClass(this, FulfillTask.class);
-                startActivity(intent);
-
-        /* Backup, trying to get it to work with ArrayList<Task>
-         * 
-        //list index set to position of clicked entry
-        MainActivity.list_index = position;
-        //find task that was clicked
-        Task clickedTask = MainActivity.thelist.get(MainActivity.list_index);
-        //get task requirements
-        String requirements = clickedTask.getDescription();
-        
-        Intent intent = new Intent (this, FulfillTask.class);
-        //send requirements with intent
-        intent.putExtra(EXTRA_MESSAGE, requirements);
+        b.putSerializable("task", clickedTask);
+        intent.putExtras(b);
+        intent.setClass(this, FulfillTask.class);
         startActivity(intent);
-        */
-    	//Attempt to create ArrayList<Task> from array Task[]
-    	ArrayList<Task> tasks = (ArrayList<Task>) Arrays.asList(readAllExtTasks());
-    	Task clickedTask = tasks.get(position);
-    	
-    	Intent intent = new Intent();
-    	Bundle b = new Bundle();
-		b.putSerializable("task", clickedTask);
-		intent.putExtras(b);
-		intent.setClass(this, FulfillTask.class);
-		startActivity(intent);
-
     }
     private ArrayList<Task> readAllExtTasks(){
         ArrayList<Task> tasks = new ArrayList<Task>();
