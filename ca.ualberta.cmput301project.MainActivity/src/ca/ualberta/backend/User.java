e1e8fd35ebcea838fdd8e58b05083ad112e1da24
pqin <pqin@ualberta.ca>
2012-11-19 22:45:31 -0700

    new Authentication class using SQLite

--- ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/backend/User.java
+++ ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/backend/User.java
@@ -1,7 +1,6 @@
 package ca.ualberta.backend;
 
-import ca.ualberta.backend.Authentication;
-import java.sql.*;
+import android.util.Log;
 
 /** Checks username and password entered in LoginActivity,
  *  indicating logged in user by username.
@@ -13,75 +12,38 @@ import java.sql.*;
 public class User {
 	private String username;
 	private String password;
-	private Connection dbconnection;
 	private boolean LoggedIn;
-	
+
 	public User(){
-		username = "John Doe";
-		password = "12345";
-		dbconnection = null;
+		username = "john_doe@example.com";
+		password = "asdf";
 		LoggedIn = false;
+		Log.d("User", "User() constructor");
 	}
-	private void setUsername(String newname){
+	public User(String new_name, String new_password){
+		username = new_name;
+		password = new_password;
+		LoggedIn = false;
+		Log.d("User", "User(String, String) constructor");
+	}
+	
+	public void setUsername(String newname){
 		username = newname;
 	}
-	private void setPassword(String newpassword){
+	public void setPassword(String newpassword){
 		password = newpassword;
 	}
-	private void setStatus(boolean login_status){
+	public void setLoggedIn(boolean login_status){
 		LoggedIn = login_status;
 	}
+	
 	public String getUsername(){
 		return username;
 	}
 	public String getPassword(){
 		return password;
 	}
-	public boolean getStatus(){
+	public boolean getLoggedIn(){
 		return LoggedIn;
-	}
-	public String Register(String username, String password){
-		String status = "";
-		try {
-			if (Authentication.createNewUser(dbconnection, username, password)){
-				status = "Created new user \'" + username + "\'.";
-			} else {
-				status = "User \'" + username + "\' already exists.";
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			status = "SQL Exception occurred";
-		}
-		return status;
-	}
-	public String Login(String username, String password){
-		String status = "";
-		try {
-			if (Authentication.logIn(dbconnection, username, password)){
-				if (getStatus() == false){
-					status = "\'" + username + "\' login successful";
-					setUsername(username);
-					setPassword(password);
-					setStatus(true);
-				} else {
-					status = "Already logged in.";
-				}
-			} else {
-				status = "\'" + username + "\' login failed.";
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			status = "SQL Exception occurred";
-		}
-		return status;
-	}
-	public void Logout(){
-		if (getStatus()){
-			setUsername("");
-			setPassword("");
-			setStatus(false);
-		}
 	}
 }
--- ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/frontend/LoginActivity.java
+++ ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/frontend/LoginActivity.java
@@ -1,46 +1,68 @@
 package ca.ualberta.frontend;
 
-import ca.ualberta.backend.User;
 import ca.ualberta.frontend.R;
+import ca.ualberta.backend.SQLiteAuthentication;
+import ca.ualberta.backend.User;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.app.Activity;
 
-/** User interface for app user to register as new user or login as existing user
+/** User interface for app user to register as new user or login as existing user.
  * 
  * @author pqin
  *
  */
 public class LoginActivity extends Activity implements OnClickListener {
-	User user = new User();
-    @Override
+	private SQLiteAuthentication db = new SQLiteAuthentication(this);
+	private User user = new User();
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("Login", "Called super.onCreate()");
         setContentView(R.layout.activity_login);
         Log.d("Login", "setContentView Layout");
+        
         Button loginUserButton = (Button) findViewById(R.id.userLoginButton);
         Button registerUserButton = (Button) findViewById(R.id.userRegisterButton);
         loginUserButton.setOnClickListener(this);
         registerUserButton.setOnClickListener(this);
     }
+    @Override
+    public void onStop(){
+    	db.close();
+    }
 
 	public void onClick(View v) {
 		EditText nameField = (EditText) findViewById(R.id.userField);
 		EditText passField = (EditText) findViewById(R.id.passField);
+		TextView statusField = (TextView) findViewById(R.id.loginStatus);
 		String username = nameField.getText().toString();
 		String password = passField.getText().toString();
-		
+		user.setUsername(username);
+		user.setPassword(password);
+
 		switch(v.getId()){
 			case R.id.userRegisterButton:
-				user.Register(username, password);
+				if (db.AddRecord(user) == 0){
+					statusField.setText("New user registered.");
+				} else {
+					statusField.setText("Failed to register new user.");
+				}
 				break;
 			case R.id.userLoginButton:
-				user.Login(username, password);
+				if (db.FindRecord(user) == 0){
+					statusField.setText("User logged in.");
+					user.setLoggedIn(true);
+				} else {
+					statusField.setText("Login failure.");
+					user.setLoggedIn(false);
+				}
 				break;
 			default:
 				break;
