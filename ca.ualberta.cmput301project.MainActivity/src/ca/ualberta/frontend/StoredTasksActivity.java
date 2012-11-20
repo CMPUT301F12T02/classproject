fbcd0ae265dd4e05ae0a98699daf70d1fbd29830
Gabriel Caciula <gabriel.caciula@gmail.com>
2012-11-20 12:19:39 -0700

    added skeleton functionality for saving favourites and drafts

--- ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/frontend/StoredTasksActivity.java
+++ ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/frontend/StoredTasksActivity.java
@@ -17,15 +17,20 @@ public class StoredTasksActivity extends Activity implements OnClickListener {
         
         Button viewLocaldId = (Button) findViewById(R.id.local_button);
         Button viewComId = (Button) findViewById(R.id.com_button);
+        Button viewFavouritesdId = (Button) findViewById(R.id.favourites_button);
+        Button viewDraftsId = (Button) findViewById(R.id.drafts_button);
+        
         viewLocaldId.setOnClickListener(this);
         viewComId.setOnClickListener(this);
+        viewFavouritesdId.setOnClickListener(this);
+        viewDraftsId.setOnClickListener(this);
     }
         
     public void onClick(View v){
     	//Needs to get request information,
     	// to populate task description box
     	// to gray out appropriate get media button
-    	Intent intent;
+    	Intent intent = null;
     	
         switch (v.getId()){
 	        case R.id.local_button:
@@ -34,10 +39,16 @@ public class StoredTasksActivity extends Activity implements OnClickListener {
 	        case R.id.com_button:
 	        	intent = new Intent(this, ViewExternalTaskActivity.class);
 	        	break;
+	        case R.id.favourites_button:
+	        	//intent = new Intent(this, ViewFavouritesActivity.class);
+	        	break;
+	        case R.id.drafts_button:
+	        	//intent = new Intent(this, ViewDraftsActivity.class);
+	        	break;
 	        default:
 	        	intent = new Intent();
 	        	break;
         }
-            startActivity(intent);
+        startActivity(intent);
     }
 }
