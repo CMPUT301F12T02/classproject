ac5dabcd5ad00a3ab45bfbad9d77d0e39685c4ea
fessehaye <fessehay@ualberta.ca>
2012-12-03 11:38:03 -0700

    mediaupdate

--- ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/frontend/TakePhotoActivity.java
+++ ca.ualberta.cmput301project.MainActivity/src/ca/ualberta/frontend/TakePhotoActivity.java
@@ -12,55 +12,70 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class TakePhotoActivity extends Activity {
+public class TakePhotoActivity extends Activity implements OnClickListener {
 
 	Uri imageFileUri;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_takephoto);
+
+		ImageButton ibutton = (ImageButton) findViewById(R.id.TakeAPhoto);
+		Button fButton = (Button) findViewById(R.id.finishButton);
+		OnClickListener listen = new OnClickListener(){
+			public void onClick(View v) {
+				takePhoto();
+			}
+		};
+		
+		fButton.setOnClickListener(this);
+		ibutton.setOnClickListener(listen);
+	}
+
+	public void takePhoto(){   	
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+		startActivityForResult(intent, 1888);
+	}
+
 	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_takephoto);
-        
-        ImageButton button = (ImageButton) findViewById(R.id.TakeAPhoto);
-        OnClickListener listener = new OnClickListener(){
-        	public void onClick(View v){
-        		takePhoto();
-        	}
-        };
-        button.setOnClickListener(listener);
-    }
-    
-    public void takePhoto(){   	
-    	Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
 
-    	startActivityForResult(intent, 1888);
-    }
-    
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
 
-    	
-    		TextView tv = (TextView) findViewById(R.id.camera_status);
-    		switch (resultCode){
-    			case RESULT_OK:
-    				ImageButton button = (ImageButton) findViewById(R.id.TakeAPhoto);
-    				Bitmap photo = (Bitmap) data.getExtras().get("data");
-    				button.setImageBitmap(photo);
-    				tv.setText("Took photo.");
-    				
-    				
-    				break;
-    			case RESULT_CANCELED:
-    				tv.setText("Cancelled photo.");
-    				break;
-    			default:
-    				tv.setText("Not sure what happened: " + resultCode);
-    				break;
-    		
-    	}
+		TextView tv = (TextView) findViewById(R.id.camera_status);
+		switch (resultCode){
+		case RESULT_OK:
+			ImageButton button = (ImageButton) findViewById(R.id.TakeAPhoto);
+			Bitmap photo = (Bitmap) data.getExtras().get("data");
+			button.setImageBitmap(photo);
+			tv.setText("Took photo.");
 
-    }
+
+			break;
+		case RESULT_CANCELED:
+			tv.setText("Cancelled photo.");
+			break;
+		default:
+			tv.setText("Not sure what happened: " + resultCode);
+			break;
+
+		}
+
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+	switch (v.getId()){
+		case R.id.finishButton:
+			finish();
+			break;
+		
+	}
+}
 }
